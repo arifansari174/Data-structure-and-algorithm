074b7253b8c834aec64e58d7512bb363621943e6
Arif Ansari <arifansari63915@gmail.com>
2025-08-25 21:50:48 +0530

    Add files via upload

---
+++ divisible by 3and 7.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		for(int i=1; i<=30; i++)
+		{
+		   if( i%3==0 &&i%7==0)
+		   {
+		       System.out.println(i);
+		   }
+		}
+		   
+		
+	}
+}
