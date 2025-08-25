73bae0068a5dff68b9711717c1c2b8ac38d41324
Arif Ansari <arifansari63915@gmail.com>
2025-08-25 21:44:49 +0530

    Add files via upload
    Number 1to 30 which divisible by 3

---
+++ 1to30divisible by 3.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		    int i=1;
+		    while(i<=30)
+		    {
+		        if(i%3==0)
+		        {
+		            System.out.println(i);
+		        }
+		        i++;
+		    }
+	}
+}
