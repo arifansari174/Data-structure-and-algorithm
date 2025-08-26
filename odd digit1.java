0bf29b824b7f12bfce0dc69fdabb28a5e55144cb
Arif Ansari <arifansari63915@gmail.com>
2025-08-26 23:14:18 +0530

    Add files via upload
    Odd digit print in this code

---
+++ odd digit1.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		int number = 36747;
+		while(number>0){
+		    int digit = number%10;
+		    if (digit%2!=0){
+		        System.out.println(digit);
+		    }
+		    number =number/10;
+		}
+	}
+}
