eecf8ff4f08395eb707eada61c904a0eadc1535e
Arif Ansari <arifansari63915@gmail.com>
2025-08-29 22:43:15 +0530

    Add files via upload
    Sum of each even digit in number

---
+++ 21A.java
@@ -0,0 +1,16 @@
+public class sumofEachEvenDigit {
+	public static int sumofEvenDigit(int n) {
+		int sum =0;
+		while (n>0) {
+		    int d= n%10;
+		    if(d%2==0) {
+		        sum =sum+d;
+		    }
+		    n =n/10;
+		}
+		return sum;
+	}
+	public static void main(String[]args){
+	    System.out.println(sumofEvenDigit(12345));
+	}
+}
