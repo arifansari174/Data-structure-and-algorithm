20ba56d1795a037daab868fe69de487ec2da4fe4
Arif Ansari <arifansari63915@gmail.com>
2025-08-29 22:48:12 +0530

    Add files via upload
    Factorial by using method

---
+++ factorial 22.java
@@ -0,0 +1,13 @@
+public class factorialByMethod {
+	public static int factorial (int n) {
+		int fact =1;
+		while(n>0){
+		    fact =fact*n;
+		    n--;
+		}
+		return fact;
+	}
+	public static void main(String[]args){
+	    System.out.println(factorial(5));
+	}
+}
