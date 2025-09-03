2997ee815faa3eeefa46f21d0fa9f06fea46ed64
Arif Ansari <arifansari63915@gmail.com>
2025-09-03 22:46:49 +0530

    Add files via upload

---
+++ Diamond shape Number pattern.java
@@ -0,0 +1,33 @@
+public class NumberPattern6 {
+    public static void main(String[] args) {
+        int n = 4;  // middle row size
+
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        // lower half
+        for (int i = n - 1; i >= 1; i--) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
