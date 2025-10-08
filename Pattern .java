074fd7b63155bc993da737c7409dcb5af9a0c1e1
Arif Ansari <arifansari63915@gmail.com>
2025-10-08 23:36:11 +0530

    Add files via upload

--- Pattern .java
+++ Pattern .java
@@ -1,11 +1,12 @@
 package com.Pattern;
 
-public class Pattern3 {
+public class Pattern5 {
 public static void main(String[] args) {
-        int n=5;
-        for(int i=1; i<=5; i++) {
-            for(int j=1; j<=5; j++) {
-                if(i+j>=n+1) {
+        
+	int n = 9;
+        for(int i=1; i<=9; i++) {
+            for(int j=1; j<=9; j++) {
+                if(i>=j && i+j<=n-1)  {
                     System.out.print(" *");
                 }
                 else{
--- Pattern.java
+++ Pattern.java
@@ -1,11 +1,12 @@
 package com.Pattern;
 
-public class Pattern2 {
+public class Pattern6 {
 public static void main(String[] args) {
         
-        for(int i=1; i<=5; i++) {
-            for(int j=1; j<=5; j++) {
-                if(i<=j) {
+	int n=9;
+        for(int i=1; i<=9; i++) {
+            for(int j=1; j<=9; j++) {
+                if(i<=j && i+j<=n-1)  {
                     System.out.print(" *");
                 }
                 else{
