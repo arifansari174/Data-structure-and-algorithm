d4bcd3540b02dad83ffc1a42e61297c23858909a
Arif Ansari <arifansari63915@gmail.com>
2025-11-18 18:23:20 +0530

    Update Duplicate.java

--- Duplicate.java
+++ Duplicate.java
@@ -10,7 +10,7 @@ public class Duplicates {
                 if (arr[i] == arr[j]) {
                     System.out.println(arr[i]);
                     break; 
-                }
+				}
             }
         }
     }
